a0f51081626bdf870d390d77205157969a1ea4b5
frank.gairal <frank.gairal@fullsix.com>
2015-10-27 12:51:41 +0700

    Feat: factorise ftp task

--- gulp/tasks/deploy.js
+++ gulp/tasks/deploy.js
@@ -10,7 +10,7 @@ var knownOptions = {
   default: {env: 'local'}
 };
 var options = minimist(process.argv.slice(2), knownOptions);
-var host = 'ftp.gairal.com', dest;
+var host = 'ftp.gairal.com', src, dest;
 
 switch(options.env) {
     case 'prod':
@@ -21,7 +21,7 @@ switch(options.env) {
         break;
 }
 
-gulp.task('ftp:build', function() {
+gulp.task('ftp', function() {
     return gulp.src('.')
     .pipe(prompt.prompt([{
         type: 'input',
@@ -40,35 +40,7 @@ gulp.task('ftp:build', function() {
             pass: res.pass,
             parallel: 10
         });
-        gulp.src(conf.base.build + '/**/*')
-        .pipe(conn.newer(dest))
-        .pipe(conn.dest(dest))
-        .on('error', function(err) {
-            console.log(err);
-        });
-    }));
-});
-
-gulp.task('ftp:compile', function() {
-    return gulp.src('.')
-    .pipe(prompt.prompt([{
-        type: 'input',
-        name: 'user',
-        message: 'Enter ftp user'
-    },
-    {
-        type: 'password',
-        name: 'pass',
-        message: 'Enter ftp password'
-    }]
-    ,function(res){
-        var conn = ftp.create({
-            host: host,
-            user: res.user,
-            pass: res.pass,
-            parallel: 10
-        });
-        gulp.src(conf.base.compile + '/**/*')
+        gulp.src(src)
         .pipe(conn.newer(dest))
         .pipe(conn.dest(dest))
         .on('error', function(err) {
@@ -78,9 +50,11 @@ gulp.task('ftp:compile', function() {
 });
 
 gulp.task('deploy:build', function (cb){
-    runSequence('build', 'ftp:build', cb);
+    src = conf.base.build + '/**/*';
+    runSequence('build', 'ftp', cb);
 });
 
 gulp.task('deploy:compile', function (cb){
-    runSequence('compile', 'ftp:compile', cb);
+    src = conf.base.compile + '/**/*';
+    runSequence('compile', 'ftp', cb);
 });
